f2f09336bb7dc10067cc4141aa3b0d954a57fb76
Shamir Torres Villamil <shamirtv1@gmail.com>
2025-02-14 09:12:01 -0500

    Config env.template and RpcCustonFilter

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,59 +1,81 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
-
   private readonly logger = new Logger('ProductsService');
 
   onModuleInit() {
-    
     this.$connect();
-    this.logger.log('Database connected')
+    this.logger.log('Database connected');
   }
 
   create(createProductDto: CreateProductDto) {
-    return this.product.create({ data: createProductDto })
+    return this.product.create({ data: createProductDto });
   }
 
   async findAll(paginationDto: PaginationDto) {
-    
     const { page, limit } = paginationDto;
-    const totalPage = await this.product.count({ where: {  available: true } });
-    const lastPage = Math.ceil( totalPage / limit );
+    const totalPage = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalPage / limit);
 
     return {
-       data: await this.product.findMany({
-        skip: ( page - 1 ) * limit,
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
         take: limit,
-        where: {  available: true }
+        where: { available: true },
       }),
-      meta: { total: totalPage, page: page, lastPage: lastPage }
-    } 
-    
-
+      meta: { total: totalPage, page: page, lastPage: lastPage },
+    };
   }
 
   async findOne(id: number) {
-    const product = await this.product.findFirst({ where: { id, available: true } });
-    if( !product ) throw new NotFoundException(`Product with id: #${ id } not found`);
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
+    if (!product) {
+      throw new RpcException({
+        message: `Product with id: #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
     return product;
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
-
     const { id: ___, ...restData } = updateProductDto;
-
     await this.findOne(id);
-    return this.product.update({ where:{ id }, data: restData })
+    return this.product.update({ where: { id }, data: restData });
   }
 
   async remove(id: number) {
     await this.findOne(id);
-    const product = await this.product.update({ where: { id }, data: { available: false } });
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
     return product;
+  }
+
+  async validateProducts(ids: number[]) {
+
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: { id: { in: ids } },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Some product were not found`,
+      });
+    }
+
+    return products
   }
 }
